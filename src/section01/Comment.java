2faafa33849f7865733c8b512675accb5bcbf6c0
KMS <fj7910@naver.com>
2023-08-06 09:48:26 +0900

    java

---
+++ src/section01/Comment.java
@@ -0,0 +1,20 @@
+package section01;
+/*
+ * 주석 
+ *  코드에 영향을 주지 않는 개발자가 사용하는 메모공간
+ *  
+ *여러줄 주석
+ *
+ * Ctrl + d : 줄삭제
+ *  
+ *Ctrl + F11 : 실행단축기 
+ */
+public class Comment {
+	public static void main(String[] args) {
+		// 한줄 주석
+		
+			}
+
+}
+
+
