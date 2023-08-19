37319df6fd2886164c676f7a2575273536c492de
KMS <fj7910@naver.com>
2023-08-19 09:36:42 +0900

    loop

--- src/section05/LoopHomeWork.java
+++ src/section05/LoopHomeWork.java
@@ -39,21 +39,65 @@ package section05;
 */
 public class LoopHomeWork {
 	public static void main(String[] args) {
-//		for(int i = 0; i < 7; i++) {
-//			for(int j = 0; j < i+1; j++) {
-//				System.out.print("*");
-//			}
-//			System.out.println();
-//		}
-//		System.out.println();
 		
-		for(int i = 0; i < 4; i++) {
+		for(int i = 0; i < 7; i++) {
 			for(int j = 0; j < i+1; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-
+		System.out.println();
+		
+		
+		System.out.println("1=======================");
+		
+		for(int i = 0; i<5; i++) {
+			for(int j = 0; j<5-i; j++) {
+				System.out.print(" ");
+			}
+		    for(int j = 0; j < i; j++) {
+			    System.out.print("*");
+			
+		    }
+		System.out.println();
+		}
+		System.out.println("2=======================");
+		
+		for(int i = 1; i <= 4; i++) {
+			for(int j = 1; j <= 4-i; j++) {
+				System.out.print(" ");
+			}
+		for(int k = 1; k <= i*2-1; k++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		}
+		System.out.println("======================================");
+		for(int i = 0; i < 7; i++) {
+			
+			for(int j = 0; j < 7; j++) {
+				if(i <= 7/2) {
+					if(j < 3 - i) {
+						System.out.print(" ");
+					}else if (j > 3 + i) {
+						System.out.print(" ");
+					} else {
+						System.out.print("*");
+					}
+				} else {
+					if (j < i + 3) {
+						System.out.print(" ");
+					} else if ( j > 9 - i) {
+						System.out.print(" ");
+					} else {
+						System.out.print("*");
+					} 
+				}
+			}	
+			System.out.println();
+		}
+		
 	}
-
+	
+	
 }
--- src/section05/LoopHomeWork2.java
+++ src/section05/LoopHomeWork2.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package section05;
 /*
 구구단
@@ -35,5 +38,22 @@ package section05;
 
  */
 public class LoopHomeWork2 {
+	public static void main(String[] args) {
+		for (int k = 0; k < 3; k++) {
+			for (int i = 0; i < 9; i++) {
+				for(int j = 0; j < 3; j++) {
+					int dan = j+2 + (k*3);
+					int mul = i+1;
+					int result = dan * mul;
+					
+					if (dan < 10)
+					System.out.printf("%d x %d = %d\t", dan, mul, result);
+				}
+				System.out.println();
+			}
+		    System.out.println();
+		}
+		
+	}
 
 }
