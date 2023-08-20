e5d9753c3a024eeda4a4fe4445aa7052cd1380b6
KMS <fj7910@naver.com>
2023-08-20 12:07:43 +0900

    con

---
+++ src/section09/Beer.java
@@ -0,0 +1,24 @@
+package section09;
+
+public class Beer {
+	//
+	int volume;
+	String brand;
+	String type;
+	int price;
+	
+	public Beer(int volume, String brand, String type, int price) {
+		this.volume = volume;
+		this.brand = brand;
+		this.type = type;
+		this.price = price;
+		
+	}
+	public void getInfo() {
+		System.out.println("volume: " + volume);
+		System.out.println("brand: " + brand);
+		System.out.println("type: " + type);
+		System.out.println("price: " + price);
+	}
+
+}
